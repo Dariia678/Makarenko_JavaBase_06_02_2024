7bc9850b27996018c77cf1c0d3105b259070c7b6
Makarenko Dariia <vermicell.i678@gmail.com>
2024-02-15 13:43:29 +0100

    Testing commit + push

---
+++ src/main/java/ua/hillel/makarenko/lessons/lesson3/Main.java
@@ -0,0 +1,63 @@
+package ua.hillel.makarenko.lessons.lesson3;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "Ingredients on how to make pancakes";
+        System.out.println(str);
+
+        byte number = 2;
+        System.out.println(number);
+
+        String str1 = "eggs";
+        System.out.println(str1);
+
+        byte number1 = 2;
+        System.out.println(number1);
+
+        String str2 = "glasses milk";
+        System.out.println(str2);
+
+        double number2 = 2.5;
+        System.out.println(number2);
+
+        String str3 = "glasses flout";
+        System.out.println(str3);
+
+        byte number3 = 3;
+        System.out.println(number3);
+
+        String str4 = "tablespoons sugar";
+        System.out.println(str4);
+
+        byte number4 = 1;
+        System.out.println(number4);
+
+        String str5 = "glass boiling water";
+        System.out.println(str5);
+
+        byte number5 = 3;
+        System.out.println(number5);
+
+        String str6 = "tablespoons sugar";
+        System.out.println(str6);
+
+        double number6 = 0.5;
+        System.out.println(number6);
+
+        String str7 = "teaspoon baking soda";
+        System.out.println(str7);
+
+        String str8 = "teaspoon salt";
+        System.out.println(str8);
+
+        System.out.println(str);
+        System.out.println(number + " " + str1);
+        System.out.println(number1 + " " + str2);
+        System.out.println(number2 + " " + str3);
+        System.out.println(number3 + " " + str4);
+        System.out.println(number4 + " " + str5);
+        System.out.println(number5 + " " + str6);
+        System.out.println(number6 + " " + str7);
+        System.out.println(number6 + " " + str8);
+    }
+}
