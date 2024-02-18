30e3a765ce318e7448fd5e3b5a07797370a876ea
Makarenko Dariia <vermicell.i678@gmail.com>
2024-02-18 16:12:10 +0100

    HW3

---
+++ src/main/java/ua/hillel/makarenko/lessons/lesson4/HW3/Main.java
@@ -0,0 +1,15 @@
+package ua.hillel.makarenko.lessons.lesson4.HW3;
+
+public class Main {
+    public static void main(String[] args) {
+        double a = 7;
+        double b = 4;
+        double c = 11;
+
+        double volume = a * b * c;
+        System.out.println("ќб'Їм паралелеп≥педа = " + volume);
+
+        double length = 2 * (a + b + c);
+        System.out.println("—умарна довжина вс≥х стор≥н = " + length);
+    }
+}
