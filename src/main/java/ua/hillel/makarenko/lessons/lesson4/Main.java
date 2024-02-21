eb929dc44160490d88b681db7b9b8567e63770da
Makarenko Dariia <vermicell.i678@gmail.com>
2024-02-21 23:08:00 +0100

    lesson4

--- src/main/java/ua/hillel/makarenko/lessons/lesson4/Main.java
+++ src/main/java/ua/hillel/makarenko/lessons/lesson4/Main.java
@@ -3,6 +3,7 @@ package ua.hillel.makarenko.lessons.lesson4;
 public class Main {
     public static void main(String[] args) {
 
+//        Пример:
 //        int a = 5;
 //        int b = 10;
 
@@ -10,19 +11,20 @@ public class Main {
 //        System.out.println(a - b);
 //        System.out.println(a * b);
 //        System.out.println(b / a);
-//
+
 //        System.out.println(10.0 / 3);
 //        System.out.println(10 / 3.0);
 //        System.out.println(10.0/ 3.0);
 //        System.out.println(10D / 3);
-//
+
 //        System.out.println((double) b / a);
-//
+
+//        Пример:
 //        System.out.println(8 % 3); // 2 * 3 = 6. 8 - 6 = 2
 //        System.out.println(9 % 4); // 2 * 4 = 8. 9 - 8 = 1
-//
+
 //        System.out.println();
-//
+
 //        System.out.println(10 % 1); // 10 * 1 = 10. 10 - 10 = 0
 //        System.out.println(10 % 2); // 5 * 2 = 10. 10 - 10 = 0
 //        System.out.println(10 % 3); // 3 * 3 = 9. 10 - 9 = 1
@@ -35,116 +37,130 @@ public class Main {
 //        System.out.println(10 % 10); // 1 * 10 = 10. 10 - 10 = 0
 //        System.out.println(10 % 11); // 10
 
+//        Пример:
 //        System.out.println("rjjfeuirfos\nenhrfehbfdehnfrsdhnefoswnhrfsdnhfr\todbhfrswdehnfoswfrhn" +
 //                "swbhfsdhbfoswnhfrosdnhrflosh" +
 //                "bfroshrnflosdhrf");
+
+//        Пример:
 //        int x = 10;
 //        int y = x + 5;
-//
+
 //        x = y;
-//
+
 //        System.out.println(x);
 
+//        Пример:
 //        int x = 10;
-//
 //        x = x + 5;
 
 //        System.out.println(x);
 
+//        Пример:
 //        int x = 10;
-//
+
 //        x += 5;
 //        x -= 5;
 //        x *= 5;
 //        x /= 5;
 //        x %= 5;
-//
+
 //        System.out.println(x);
 
+//        Пример:
 //        int x = 10;
-//
+
 //        x ++;
 //        x --;
-//
+
 //        System.out.println(x);
+
+//        Пример:
 //        int x = 10;
-//
+
 //        x = x + 1;
 //        x += 1;
 //        x++;
 //        ++x;
-//
+
 //        System.out.println(x++);
 //        System.out.println(++x);
+
+//        Пример:
 //         int a = 5;
 //         int b = 2;
 //         int result = a++ + b++;
-//
+
 //        System.out.println("result = " + result);
 //        System.out.println("a = " + a);
 //        System.out.println("b = " + b);
-//
+
 //        System.out.println(a + b);
 
+//        Пример:
 //        int a = 5;
 //        int b = 2;
 //        int result = --a + b++ + a++;
-//
+
 //        System.out.println("result = " + result);
 //        System.out.println("a = " + a);
 //        System.out.println("b = " + b);
 
+//        Пример:
 //        int a = 5;
 //        int b = 2;
 //        int result = ++a + a++ - b++;
-//
+
 //        System.out.println("result = " + result);
 //        System.out.println("a = " + a);
 //        System.out.println("b = " + b);
+
+//        Пример:
 //        int a = 5;
 //        int b = 2;
 //        int result = --a + ++a + ++b;
-//
+
 //        System.out.println("result = " + result);
 //        System.out.println("a = " + a);
 //        System.out.println("b = " + b);
-//
+
 //        System.out.println(Math.PI);
-//
+
+//        Пример:
 //        int absValue = Math.abs(10);
 //        System.out.println(absValue);
 //        System.out.println(Math.abs(10));
 
-        System.out.println(Math.pow(2, 3));
+//        System.out.println(Math.pow(2, 3));
+//
+//        System.out.println(Math.sqrt(16));
 
-        System.out.println(Math.sqrt(16));
+//        System.out.println(Math.round(11.1));
+//        System.out.println(Math.round(11.6));
 
-        System.out.println(Math.round(11.1));
-        System.out.println(Math.round(11.6));
+//        System.out.println(Math.max(11, 5));
+//        System.out.println(Math.min(11, 5));
 
-        System.out.println(Math.max(11, 5));
-        System.out.println(Math.min(11, 5));
+//        System.out.println(Math.random());
+//        System.out.println(Math.random());
 
-        System.out.println(Math.random());
-        System.out.println(Math.random());
+//        System.out.println(Math.random() * 10);
 
-        System.out.println(Math.random() * 10);
+//        От 0.0 до 0.999999
+//        System.out.println((int) (Math.random() * 10));
+//        System.out.println((int) (Math.random() * 10));
+//        От 0.0 до 10
+//        System.out.println((int) (Math.random() * 11));
+//        System.out.println(Math.round(Math.random() * 10));
 
-//      От 0.0 до 0.999999
-        System.out.println((int) (Math.random() * 10));
-        System.out.println((int) (Math.random() * 10));
-//      От 0.0 до 10
-        System.out.println((int) (Math.random() * 11));
-        System.out.println(Math.round(Math.random() * 10));
+//        От 15 до 30
+//        System.out.println((int) (Math.random() * 16) + 15);
 
-//      От 15 до 30
-        System.out.println((int) (Math.random() * 16) + 15);
-
-        int min = 15;
-        int max = 30;
-//      Формула
-        int result = (int) (Math.random() * (max - min + 1)+ min);
-        System.out.println((int) (Math.random() * (max - min + 1) + min));
-        System.out.println(result);
+//        int min = 15;
+//        int max = 30;
+//        Пример:
+//        int result = (int) (Math.random() * (max - min + 1)+ min);
+//        System.out.println((int) (Math.random() * (max - min + 1) + min));
+//        System.out.println(result);
     }
 }
