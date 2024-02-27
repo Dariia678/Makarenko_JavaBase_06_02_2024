17afab11586ac2e0efe9a527d03b5133130f1e08
Makarenko Dariia <vermicell.i678@gmail.com>
2024-02-27 18:41:25 +0100

    HW5

---
+++ src/main/java/ua/hillel/makarenko/lessons/lesson6/HW5/Main.java
@@ -0,0 +1,35 @@
+package ua.hillel.makarenko.lessons.lesson6.HW5;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+        public static void main(String[] args) {
+            Random rand = new Random();
+            int numberToGuess = rand.nextInt(11);
+            int attempts = 3;
+
+            System.out.println("Комп'ютер загадав число від 0 до 10. Спробуйте відгадати!");
+
+            Scanner input = new Scanner(System.in);
+
+            while (attempts > 0) {
+                System.out.print("Введіть вашу спробу: ");
+                int userGuess = input.nextInt();
+
+                if (userGuess == numberToGuess) {
+                    System.out.println("Вітаємо! Ви відгадали число!");
+                    break;
+                } else {
+                    System.out.println("Неправильно. Спробуйте ще раз.");
+                    attempts--;
+                }
+            }
+
+            if (attempts == 0) {
+                System.out.println("На жаль, ви не відгадали число. Загадане число було: " + numberToGuess);
+            }
+
+            input.close();
+        }
+    }
