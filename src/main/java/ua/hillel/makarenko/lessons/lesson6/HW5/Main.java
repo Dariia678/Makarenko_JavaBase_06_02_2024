6aeeb0753b5dd5a283f45c37eb34e237d6e7f4ea
Makarenko Dariia <vermicell.i678@gmail.com>
2024-02-28 11:18:29 +0100

    HW5

--- src/main/java/ua/hillel/makarenko/lessons/lesson6/HW5/Main.java
+++ src/main/java/ua/hillel/makarenko/lessons/lesson6/HW5/Main.java
@@ -4,32 +4,41 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-        public static void main(String[] args) {
-            Random rand = new Random();
-            int numberToGuess = rand.nextInt(11);
-            int attempts = 3;
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int numberToGuess = rand.nextInt(11);
+        int attempts = 3;
 
-            System.out.println("Комп'ютер загадав число від 0 до 10. Спробуйте відгадати!");
+        System.out.println("Комп'ютер загадав число від 0 до 10. Спробуйте відгадати!");
 
-            Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-            while (attempts > 0) {
-                System.out.print("Введіть вашу спробу: ");
-                int userGuess = input.nextInt();
+        while (attempts > 0) {
+            System.out.print("Введіть вашу спробу: ");
 
-                if (userGuess == numberToGuess) {
-                    System.out.println("Вітаємо! Ви відгадали число!");
+            while (true) {
+                if (input.hasNextInt()) {
+                    numberToGuess = input.nextInt();
                     break;
                 } else {
-                    System.out.println("Неправильно. Спробуйте ще раз.");
-                    attempts--;
+                    System.out.println("Виникла помилка. Перезапустіть, будь ласка, програму.");
+                    input.nextLine();
+                    System.exit(0);
                 }
             }
-
-            if (attempts == 0) {
-                System.out.println("На жаль, ви не відгадали число. Загадане число було: " + numberToGuess);
+            if (attempts == numberToGuess) {
+                System.out.println("Вітаємо! Ви відгадали число!");
+                break;
+            } else {
+                System.out.println("Неправильно. Спробуйте ще раз.");
+                attempts--;
             }
-
-            input.close();
         }
+        if (attempts == 0) {
+            System.out.println("На жаль, ви не відгадали число. Загадане число було: " + numberToGuess);
+        }
+
+        input.close();
+
     }
+}
