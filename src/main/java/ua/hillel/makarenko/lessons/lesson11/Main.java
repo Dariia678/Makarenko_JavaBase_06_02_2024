4de6735ae0bf42c3e083d937748813878d6eae29
Makarenko Dariia <vermicell.i678@gmail.com>
2024-03-17 23:35:09 +0100

    lesson11

---
+++ src/main/java/ua/hillel/makarenko/lessons/lesson11/Main.java
@@ -0,0 +1,86 @@
+package ua.hillel.makarenko.lessons.lesson11;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        Пример:
+//        Person den = new Person();
+
+//        System.out.println(den.age);
+//        System.out.println(den.name);
+//        System.out.println();
+
+//        den.age = 15;
+//        den.name = "Den";
+
+//        System.out.println(den.age);
+//        System.out.println(den.name);
+
+//        den.age = 30;
+//        System.out.println("new age = " + den.age);
+
+//        Пример:
+//        Person den = new Person();
+//        den.age = 15;
+//        den.name = "Den";
+
+//        Person alex = new Person();
+//        alex.age = 22;
+//        alex.name = "Alex";
+
+//        System.out.println("den age = " + den.age);
+//        System.out.println("alex age = " + alex.age);
+
+//        Пример:
+//        Person den = new Person();
+//        den.age = 15;
+//        den.name = "Den";
+
+//        den.run();
+
+//        Person alex = new Person();
+//        alex.age = 22;
+//        alex.name = "Alex";
+
+//        alex.run();
+
+//        Пример:
+//        Person den = new Person();
+
+//        den.age = 20;
+//        den.name = "Den";
+
+//        den.run();
+//        den.swim();
+//        System.out.println("name = " + den.getName());
+//        den.increaseAge();
+//        System.out.println(den.age);
+
+//        den.demo();
+//        den.demo2();
+
+//        Пример:
+//        Person alex = new Person("Alex", 22);
+
+//        System.out.println(alex.age);
+//        System.out.println(alex.name);
+
+//        Пример:
+//        Person person = new Person("Alex");
+
+//        Пример:
+//        Person person = new Person(22);
+
+//        Пример:
+//        Person alex = new Person("Alex", 22);
+
+//        System.out.println(alex.getInfo());
+
+//        System.out.println(alex);
+
+
+
+
+
+    }
+}
