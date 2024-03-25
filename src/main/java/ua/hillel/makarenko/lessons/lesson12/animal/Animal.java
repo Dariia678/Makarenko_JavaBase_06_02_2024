b26e2cdcf4b9d1058c3d3de9fa02501c3faa80f8
Makarenko Dariia <vermicell.i678@gmail.com>
2024-03-25 19:32:02 +0100

    animal

---
+++ src/main/java/ua/hillel/makarenko/lessons/lesson12/animal/Animal.java
@@ -0,0 +1,10 @@
+package ua.hillel.makarenko.lessons.lesson12.animal;
+
+public class Animal {
+    String name;
+    int age;
+
+    void run() {
+        System.out.println(name + " run...");
+    }
+}
