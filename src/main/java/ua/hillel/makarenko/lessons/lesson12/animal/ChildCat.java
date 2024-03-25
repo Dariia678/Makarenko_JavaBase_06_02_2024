8fcee25b3328a74ebc56cc7f57fefef341d1becb
Makarenko Dariia <vermicell.i678@gmail.com>
2024-03-25 19:30:51 +0100

    animal

---
+++ src/main/java/ua/hillel/makarenko/lessons/lesson12/animal/ChildCat.java
@@ -0,0 +1,10 @@
+package ua.hillel.makarenko.lessons.lesson12.animal;
+
+public class ChildCat extends Cat{
+
+    void meow() {
+        System.out.println(name + " color " + color + " meow!");
+    }
+
+
+}
