9c3d193ef8fb71220e80cecaa7158194a98bb6d7
Makarenko Dariia <vermicell.i678@gmail.com>
2024-03-25 19:29:23 +0100

    demo

---
+++ src/main/java/ua/hillel/makarenko/lessons/lesson12/demo/Class3.java
@@ -0,0 +1,12 @@
+package ua.hillel.makarenko.lessons.lesson12.demo;
+
+public class Class3 extends Class2 {
+    public Class3(String str) {
+        super(str);
+    }
+
+    public Class3() {
+        super("werwerwer");
+        System.out.println("FROM CLASS 3");
+    }
+}
