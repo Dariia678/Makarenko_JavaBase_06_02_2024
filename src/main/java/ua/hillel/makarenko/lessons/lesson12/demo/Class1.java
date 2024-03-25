c0b647a3f2da8821dbe7468db9cf1ba0fe3050f3
Makarenko Dariia <vermicell.i678@gmail.com>
2024-03-25 19:29:37 +0100

    demo

---
+++ src/main/java/ua/hillel/makarenko/lessons/lesson12/demo/Class1.java
@@ -0,0 +1,7 @@
+package ua.hillel.makarenko.lessons.lesson12.demo;
+
+public class Class1 {
+    public Class1() {
+        System.out.println("FROM CLASS 1");
+    }
+}
---
+++ src/main/java/ua/hillel/makarenko/lessons/lesson12/demo/Class2.java
@@ -0,0 +1,8 @@
+package ua.hillel.makarenko.lessons.lesson12.demo;
+
+public class Class2 extends Class1{
+    public Class2(String str) {
+        super();
+        System.out.println("FROM CLASS 2");
+    }
+}
