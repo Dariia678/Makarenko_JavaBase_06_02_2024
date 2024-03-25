ab195a70aa31f0bf892d31aff18ac86e6d261410
Makarenko Dariia <vermicell.i678@gmail.com>
2024-03-25 19:31:34 +0100

    animal

---
+++ src/main/java/ua/hillel/makarenko/lessons/lesson12/animal/Cat.java
@@ -0,0 +1,12 @@
+package ua.hillel.makarenko.lessons.lesson12.animal;
+
+public class Cat extends Animal{
+
+    String color;
+
+    @Override
+    void run() {
+        super.run();
+        System.out.println(name + " color " + color + " run!!!");
+    }
+}
