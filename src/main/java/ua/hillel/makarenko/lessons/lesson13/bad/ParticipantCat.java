f9a8d7204b01de54a2fc6a952aeabdca72686f16
Makarenko Dariia <vermicell.i678@gmail.com>
2024-03-28 15:21:51 +0100

    bad

---
+++ src/main/java/ua/hillel/makarenko/lessons/lesson13/bad/ParticipantCat.java
@@ -0,0 +1,13 @@
+package ua.hillel.makarenko.lessons.lesson13.bad;
+
+public class ParticipantCat {
+    private String name;
+
+    public ParticipantCat(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
